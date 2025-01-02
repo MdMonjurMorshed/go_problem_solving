7ca67e6d638fd188e8caaa5c5816a15c879ce44d
MD MONJUR MORSHED <mmt3719@gmail.com>
2025-01-02 23:51:35 +0600

    reverse vowel problem

---
+++ reverse_vowel_problem.go
@@ -0,0 +1,36 @@
+package main
+
+import "strings"
+
+func ReverseVowelProblemLeetcode345(s string) string {
+	arr_str := []rune(s)
+	vowel := "AEIOUaeiou"
+	left := 0
+	right := len(arr_str) - 1
+
+	for i := 0; i < len(arr_str); i++ {
+		if left < right {
+			if !strings.Contains(vowel, string(arr_str[left])) {
+				left += 1
+			}
+			if !strings.Contains(vowel, string(arr_str[right])) {
+				right -= 1
+			}
+			if strings.Contains(vowel, string(arr_str[left])) && strings.Contains(vowel, string(arr_str[right])) {
+				arr_str[left], arr_str[right] = arr_str[right], arr_str[left]
+				left += 1
+				right -= 1
+			}
+		}
+	}
+
+	return string(arr_str)
+
+}
+
+func main() {
+	input := "IceCreAm"
+	result := ReverseVowelProblemLeetcode345(input)
+	print(result)
+
+}
